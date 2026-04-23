4e449ce5476f73a8cd5c34ac49b8e950a7280269
aani72 <suhanit120@gmail.com>
2026-04-24 00:50:52 +0530

    Add files via upload

--- Assignment Event Management/static/event.js
+++ Assignment Event Management/static/event.js
@@ -1,1 +1,17 @@
+function validateForm() {
+    let name = document.forms["form"]["name"].value;
+    if (name == "") {
+        alert("Name required");
+        return false;
+    }
+}
 
+function searchEvent() {
+    let input = document.getElementById("search").value.toLowerCase();
+    let cards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < cards.length; i++) {
+        let text = cards[i].innerText.toLowerCase();
+        cards[i].style.display = text.includes(input) ? "block" : "none";
+    }
+}
